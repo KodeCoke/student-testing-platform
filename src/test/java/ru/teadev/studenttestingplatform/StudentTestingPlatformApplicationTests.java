5be32873b9fd837fe6c551ef1982ff67a057d692
Dmitry <kovalenko@teadev.ru>
2022-11-26 17:34:38 +0400

    Initial commit

---
+++ src/test/java/ru/teadev/studenttestingplatform/StudentTestingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.teadev.studenttestingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class StudentTestingPlatformApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
